4234b6ea5b33382322cbef6bb2b66aee52e8bdc7
MVasuki1 <vasukimuthuswamy140@gmail.com>
2023-09-01 09:17:41 +0530

    Friday 01 September 2023 09:17:41 AM IST

--- find_new_showrss.py
+++ find_new_showrss.py
@@ -7,19 +7,19 @@ from datetime import datetime
 date_format = "%a, %d %b %Y %H:%M:%S %z"
 today = datetime.now(pytz.utc)
 shows = []
-for i in range(1,100):
+for i in range(100,1500):
     os.system('rm out.html')
     try:
         _ = subprocess.check_output(f'wget -cq -O out.html https://showrss.info/show/{i}.rss', shell=True).decode().strip()
+    
+        pub_date = subprocess.check_output('cat out.html | grep -oP "(?<=<pubDate>).*?(?=</pub)" | head -n 1', shell=True).decode().strip()
+        
+        parsed_datetime = datetime.strptime(pub_date, date_format)
+        print(i, " " ,(today - parsed_datetime).days)
+        if (today - parsed_datetime).days < 7:
+            shows.append(f'https://showrss.info/browse/{i}')
     except:
         continue
-    
-    pub_date = subprocess.check_output('cat out.html | grep -oP "(?<=<pubDate>).*?(?=</pub)" | head -n 1', shell=True).decode().strip()
-    
-    parsed_datetime = datetime.strptime(pub_date, date_format)
-    print(i, " " ,(today - parsed_datetime).days)
-    if (today - parsed_datetime).days < 7:
-        shows.append(f'https://showrss.info/browse/{i}')
 
 for show in shows:
     print(show)
